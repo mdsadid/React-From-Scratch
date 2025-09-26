c4b7d3deb977e621aa1a6d6a473c41eb0411c739
Sadid Hasan Rakib <sadid.hasan14@gmail.com>
2025-09-27 00:15:34 +0600

    iterating through data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import {PageWrapper} from "./components/PageWrapper";
 import {Container} from "./components/Container";
 import {Header} from "./components/Header";
+import {Search} from "./components/Search";
+import {ShortList} from "./components/ShortList";
+import {PuppiesList} from "./components/PuppiesList";
+import {NewPuppyForm} from "./components/NewPuppyForm";
 
 export function App() {
   return (
     <PageWrapper>
       <Container>
         <Header/>
+        <Main/>
       </Container>
     </PageWrapper>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <div className="mt-24 grid gap-8 sm:grid-cols-2">
+        <Search/>
+        <ShortList/>
+      </div>
+      <PuppiesList/>
+      <NewPuppyForm/>
+    </main>
   );
 }
