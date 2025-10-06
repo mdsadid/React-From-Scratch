1352dafb4988182db5b4f774b68fb3b9d1b64e72
Sadid Hasan Rakib <sadid.hasan14@gmail.com>
2025-10-07 00:11:11 +0600

    form actions

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {Search} from "./components/Search";
 import {ShortList} from "./components/ShortList";
 import {PuppiesList} from "./components/PuppiesList";
 import {NewPuppyForm} from "./components/NewPuppyForm";
-import {puppies} from "./data/puppies";
+import {puppies as puppiesData} from "./data/puppies";
 import {useState} from "react";
 import {type Puppy} from "./types";
 import {LikedContext} from "./context/liked-context";
@@ -24,6 +24,7 @@ export function App() {
 function Main() {
   const [liked, setLiked] = useState<Puppy['id'][]>([]);
   const [searchQuery, setSearchQuery] = useState<string>('');
+  const [puppies, setPuppies] = useState<Puppy[]>(puppiesData);
 
   return (
     <main>
@@ -34,7 +35,7 @@ function Main() {
         </div>
         <PuppiesList searchQuery={searchQuery} puppies={puppies}/>
       </LikedContext>
-      <NewPuppyForm/>
+      <NewPuppyForm puppies={puppies} setPuppies={setPuppies}/>
     </main>
   );
 }
--- src/components/NewPuppyForm.tsx
+++ src/components/NewPuppyForm.tsx
@@ -1,7 +1,25 @@
-export function NewPuppyForm() {
+import {Dispatch, SetStateAction} from "react";
+import {type Puppy} from "../types";
+
+export function NewPuppyForm({puppies, setPuppies}: {
+  puppies: Puppy[];
+  setPuppies: Dispatch<SetStateAction<Puppy[]>>
+}) {
   return (
     <div className="mt-12 flex items-center justify-between bg-white p-8 shadow ring ring-black/5">
-      <form className="mt-4 flex w-full flex-col items-start gap-4">
+      <form
+        action={(formData: FormData) => {
+          const newPuppy: Puppy = {
+            id: puppies.length + 1,
+            name: formData.get('name') as string,
+            trait: formData.get('trait') as string,
+            imagePath: `/images/${puppies.length + 1}.jpg`
+          };
+
+          setPuppies([...puppies, newPuppy]);
+        }}
+        className="mt-4 flex w-full flex-col items-start gap-4"
+      >
         <div className="grid w-full gap-6 md:grid-cols-3">
           <fieldset className="flex w-full flex-col gap-1">
             <label htmlFor="name">Name</label>
@@ -13,7 +31,7 @@ export function NewPuppyForm() {
             />
           </fieldset>
           <fieldset className="flex w-full flex-col gap-1">
-            <label htmlFor="trait">Personality trait</label>
+            <label htmlFor="trait">Personality Trait</label>
             <input
               className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
               id="trait"
@@ -25,7 +43,7 @@ export function NewPuppyForm() {
             disabled
             className="col-span-2 flex w-full cursor-not-allowed flex-col gap-1 opacity-50"
           >
-            <label htmlFor="avatar_url">Profile pic</label>
+            <label htmlFor="avatar_url">Profile Pic</label>
             <input
               className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
               id="avatar_url"
@@ -35,10 +53,10 @@ export function NewPuppyForm() {
           </fieldset>
         </div>
         <button
-          className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
+          className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none hover:cursor-pointer"
           type="submit"
         >
-          Add puppy
+          Add Puppy
         </button>
       </form>
     </div>
